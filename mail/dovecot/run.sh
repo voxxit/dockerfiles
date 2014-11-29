1c3e8605104a52059213deb2577c9ecd9ac8f616
Joshua Delsman <voxxit@users.noreply.github.com>
2014-11-29 09:50:27 -0800

    One more time

--- mail/dovecot/run.sh
+++ mail/dovecot/run.sh
@@ -4,7 +4,8 @@ chown -R postfix:postfix /etc/postfix
 chown -R vmail:vmail /srv/vmail
 
 if [ ! -z $LOGGLY_TOKEN ]; then
-  cat > /etc/rsyslog.d/22-loggly.conf <<EOF
+  mkdir -p /etc/rsyslog.d
+  tee /etc/rsyslog.d/22-loggly.conf <<EOF
 $template LogglyFormat, "<%pri%>%protocol-version% %timestamp:::date-rfc3339% %HOSTNAME% %app-name% %procid% %msgid% [$LOGGLY_TOKEN@$LOGGLY_UID tag=\"mail\"] %msg%\n"
 *.* @@logs-01.loggly.com:514;LogglyFormat
 EOF
