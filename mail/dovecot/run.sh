0c8e5731af2f33cfccb4347c5927cbe5c7ce458e
Joshua Delsman <voxxit@users.noreply.github.com>
2014-11-29 10:35:43 -0800

    Don't be so verbose normally

--- mail/dovecot/run.sh
+++ mail/dovecot/run.sh
@@ -3,11 +3,16 @@
 chown -R postfix:postfix /etc/postfix
 chown -R vmail:vmail /srv/mail
 
+if [ ! -z $DEBUG ]; then
+  echo "auth_verbose = yes" >> /etc/dovecot/dovecot.conf
+  echo "auth_debug = yes" >> /etc/dovecot/dovecot.conf
+fi
+
 if [ ! -z $LOGGLY_TOKEN ]; then
   if [ ! -z $LOGGLY_UID ]; then
     mkdir -p /etc/rsyslog.d
 
-    echo "\$template LogglyFormat, \"<%pri%>%protocol-version% %timestamp:::date-rfc3339% %HOSTNAME% %app-name% %procid% %msgid% [$LOGGLY_TOKEN@$LOGGLY_UID tag=\"mail\"] %msg%\n\"" > /etc/rsyslog.d/22-loggly.conf
+    echo "\$template LogglyFormat, \"<%pri%>%protocol-version% %timestamp:::date-rfc3339% %HOSTNAME% %app-name% %procid% %msgid% [$LOGGLY_TOKEN@$LOGGLY_UID tag=\\\"mail\\\"] %msg%\n\"" > /etc/rsyslog.d/22-loggly.conf
     echo "*.* @@logs-01.loggly.com:514;LogglyFormat" >> /etc/rsyslog.d/22-loggly.conf
   fi
 fi
