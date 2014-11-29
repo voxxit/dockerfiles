acafa560792e7e033e574e2249d16a3e3387ac92
Joshua Delsman <voxxit@users.noreply.github.com>
2014-11-29 10:05:58 -0800

    Fixing escaping

--- mail/dovecot/run.sh
+++ mail/dovecot/run.sh
@@ -4,17 +4,20 @@ chown -R postfix:postfix /etc/postfix
 chown -R vmail:vmail /srv/mail
 
 if [ ! -z $LOGGLY_TOKEN ]; then
-  mkdir -p /etc/rsyslog.d
-  tee /etc/rsyslog.d/22-loggly.conf <<EOF
-$template LogglyFormat, "<%pri%>%protocol-version% %timestamp:::date-rfc3339% %HOSTNAME% %app-name% %procid% %msgid% [$LOGGLY_TOKEN@$LOGGLY_UID tag=\"mail\"] %msg%\n"
-*.* @@logs-01.loggly.com:514;LogglyFormat
-EOF
+  if [ ! -z $LOGGLY_UID ]; then
+    mkdir -p /etc/rsyslog.d
+
+    echo "\$template LogglyFormat, \"<%pri%>%protocol-version% %timestamp:::date-rfc3339% %HOSTNAME% %app-name% %procid% %msgid% [$LOGGLY_TOKEN@$LOGGLY_UID tag=\"mail\"] %msg%\n\"" > /etc/rsyslog.d/22-loggly.conf
+    echo "*.* @@logs-01.loggly.com:514;LogglyFormat" >> /etc/rsyslog.d/22-loggly.conf
+  fi
 fi
 
 if [ ! -z $MAILGUN_SMTP_PASSWORD ]; then
-  postconf -e \
-    smtp_sasl_password_maps="static:$MAILGUN_SMTP_USERNAME:$MAILGUN_SMTP_PASSWORD" \
-    relayhost="[smtp.mailgun.org]:587"
+  if [ ! -z $MAILGUN_SMTP_USERNAME ]; then
+    postconf -e \
+      smtp_sasl_password_maps="static:$MAILGUN_SMTP_USERNAME:$MAILGUN_SMTP_PASSWORD" \
+      relayhost="[smtp.mailgun.org]:587"
+  fi
 fi
 
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
