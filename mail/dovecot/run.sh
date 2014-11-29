f9fb2e9d6449cdda3ff72267d571da4f71bc6cfa
Joshua Delsman <voxxit@users.noreply.github.com>
2014-11-29 09:36:58 -0800

    Use the right certificate

--- mail/dovecot/run.sh
+++ mail/dovecot/run.sh
@@ -4,9 +4,8 @@ chown -R postfix:postfix /etc/postfix
 chown -R vmail:vmail /srv/vmail
 
 if [ ! -z $LOGGLY_UID && ! -z $LOGGLY_TOKEN ]; then
-  cat > /etc/rsyslog.d/loggly.conf <<EOF
+  cat > /etc/rsyslog.d/22-loggly.conf <<EOF
 $template LogglyFormat, "<%pri%>%protocol-version% %timestamp:::date-rfc3339% %HOSTNAME% %app-name% %procid% %msgid% [$LOGGLY_TOKEN@$LOGGLY_UID tag=\"mail\"] %msg%\n"
-
 *.* @@logs-01.loggly.com:514;LogglyFormat
 EOF
 fi
