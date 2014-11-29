83233fc4239a24d0cec9c2ab0532f120dd174b36
Joshua Delsman <voxxit@users.noreply.github.com>
2014-11-29 09:43:38 -0800

    Only check for one

--- mail/dovecot/run.sh
+++ mail/dovecot/run.sh
@@ -3,14 +3,14 @@
 chown -R postfix:postfix /etc/postfix
 chown -R vmail:vmail /srv/vmail
 
-if [ ! -z $LOGGLY_UID && ! -z $LOGGLY_TOKEN ]; then
+if [ ! -z $LOGGLY_TOKEN ]; then
   cat > /etc/rsyslog.d/22-loggly.conf <<EOF
 $template LogglyFormat, "<%pri%>%protocol-version% %timestamp:::date-rfc3339% %HOSTNAME% %app-name% %procid% %msgid% [$LOGGLY_TOKEN@$LOGGLY_UID tag=\"mail\"] %msg%\n"
 *.* @@logs-01.loggly.com:514;LogglyFormat
 EOF
 fi
 
-if [ ! -z $MAILGUN_SMTP_USERNAME && ! -z $MAILGUN_SMTP_PASSWORD ]; then
+if [ ! -z $MAILGUN_SMTP_PASSWORD ]; then
   postconf -e \
     smtp_sasl_password_maps="static:$MAILGUN_SMTP_USERNAME:$MAILGUN_SMTP_PASSWORD" \
     relayhost="[smtp.mailgun.org]:587"
