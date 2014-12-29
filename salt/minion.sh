079e92e0d01ffde59bcd978c58dc2227c0e1f3a2
Joshua Delsman <voxxit@users.noreply.github.com>
2014-12-29 05:37:56 -0500

    add minion ID

--- salt/minion.sh
+++ salt/minion.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 config=/etc/salt/minion
 
+echo "${SALT_MINION_ID:-`hostname`}" > /etc/salt/minion_id
+
 if [ -f $config ]; then
   sed "s/%master%/${SALT_MASTER:-'salt'}/g"            -i $config
   sed "s/%keysize%/${SALT_KEYSIZE:-'2048'}/g"          -i $config
