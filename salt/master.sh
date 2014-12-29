bb92529af677db22e2e21091a7c60c9baba2ce57
Joshua Delsman <voxxit@users.noreply.github.com>
2014-12-29 15:47:34 -0500

    using /salt

--- salt/master.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-config=/etc/salt/master
-
-if [ -f $config ]; then
-  sed "s/%interface%/${SALT_INTERFACE:-'0.0.0.0'}/g" -i $config
-  sed "s/%state_verbose%/${SALT_STATE_VERBOSE:-'False'}/g" -i $config
-fi
-
-exec salt-master -l ${SALT_LOGLEVEL:-'warning'}
