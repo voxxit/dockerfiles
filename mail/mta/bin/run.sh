230552b3f61bf1f6225d226c808923a2e0dc096e
Joshua Delsman <voxxit@users.noreply.github.com>
2015-01-17 22:11:47 +0800

    kjsldkfjlsdf

--- mail/mta/bin/run.sh
+++ mail/mta/bin/run.sh
@@ -1,24 +1,17 @@
-#!/bin/bash
-set -x
+#!/bin/bash -x
 
-mkdir -p /var/spool/postfix/etc
-cp /etc/resolv.conf /var/spool/postfix/etc/
-
-# update spamassassin rules
-/usr/local/bin/update_spamassassin_rules.sh ${RULES_FILE}
+mkdir -p /srv/mail
 
 # Set SpamAssassin options
 cat > /etc/mail/spamassassin/local.cf <<EOF
 add_header all Status _YESNO_, score=_SCORE_ required=_REQD_ tests=_TESTS_ autolearn=_AUTOLEARN_ version=_VERSION_
-rewrite_header Subject **SPAM** (_SCORE_)
-report_safe 0
 EOF
 
 # for postgresql mapping
-sed -i "s/PGSQL_HOST/${PGSQL_HOST}/" /etc/postfix/pgsql/* /etc/dovecot/dovecot-sql.conf
-sed -i "s/PGSQL_USER/${PGSQL_USER}/" /etc/postfix/pgsql/* /etc/dovecot/dovecot-sql.conf
-sed -i "s/PGSQL_PASSWORD/${PGSQL_PASSWORD}/" /etc/postfix/pgsql/* /etc/dovecot/dovecot-sql.conf
-sed -i "s/PGSQL_DBNAME/${PGSQL_DBNAME}/" /etc/postfix/pgsql/* /etc/dovecot/dovecot-sql.conf
+sed -i "s/PGSQL_HOST/${PGSQL_HOST}/" /etc/postfix/pgsql/* /etc/dovecot/dovecot-sql.conf.ext
+sed -i "s/PGSQL_USER/${PGSQL_USER}/" /etc/postfix/pgsql/* /etc/dovecot/dovecot-sql.conf.ext
+sed -i "s/PGSQL_PASSWORD/${PGSQL_PASSWORD}/" /etc/postfix/pgsql/* /etc/dovecot/dovecot-sql.conf.ext
+sed -i "s/PGSQL_DBNAME/${PGSQL_DBNAME}/" /etc/postfix/pgsql/* /etc/dovecot/dovecot-sql.conf.ext
 
 # (re-)build postfix queue
 for queue in {active,bounce,corrupt,defer,deferred,flush,hold,incoming,private,saved,trace}; do
@@ -30,9 +23,9 @@ done
 chmod 730 /var/spool/postfix/maildrop
 chmod 710 /var/spool/postfix/public
 chown -R root /etc/postfix
-chown -R vmail:vmail /srv/mail
+chown -R vmail: /srv/mail
 chmod 755 /usr/local/bin/spam_filter.sh
-chown root:root /usr/local/bin/spam_filter.sh
+chown root: /usr/local/bin/spam_filter.sh
 
 # setup grossd
 mkdir -p /var/db/gross
