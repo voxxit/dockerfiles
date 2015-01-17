2065379a7ded960c9b9ca9e6869ca83e6ccd100b
Joshua Delsman <voxxit@users.noreply.github.com>
2015-01-17 22:11:47 +0800

    kjsldkfjlsdf

--- mail/mta/bin/update_spamassassin_rules.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -xe
-
-URL=http://www.apache.org/dist/spamassassin/source/${RULES_FILE}
-
-wget http://spamassassin.apache.org/updates/GPG.KEY ${URL} ${URL}.asc ${URL}.md5 ${URL}.sha1
-sa-update --gpgkey GPG.KEY --install ${RULES_FILE}
-rm -rf ${RULES_FILE}* GPG.KEY
