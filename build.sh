7a88e2241b4d7d39f6c3aff0adc403d2b4d4162e
Joshua Delsman <voxxit@users.noreply.github.com>
2023-08-27 17:49:48 -0400

    More updates

--- build.sh
+++ build.sh
@@ -14,25 +14,33 @@ if ! command -v docker > /dev/null; then
     exit 1
 fi
 
-docker buildx ls | grep -q buildkite-builder > /dev/null
-if [ $? -ne 0 ]; then
+if ! docker buildx ls | grep -q buildkite-builder > /dev/null; then 
     echo "Creating buildx builder ..."
+
     docker buildx create \
         --use \
         --name=buildkite-builder \
         --driver=docker-container
 fi 
 
-for dir in $dockerfile_dirs; do
-    bname=$(basename "$dir")
-    
-    echo "Building: voxxit/$bname:latest ..."
+cachedir="/tmp/.buildx-cache"
 
+for context in $dockerfile_dirs; do
+    bname=$(basename "$context")
+    image="voxxit/$bname:latest"
+    
+    echo "Building: $image ..."
+
+    # Build the image:
     docker buildx build \
         --load \
         --pull \
-        --tag "voxxit/$bname:latest" \
-        --cache-to type=local,dest=/tmp/.buildx-cache \
-        --cache-from type=local,src=/tmp/.buildx-cache \
-        "$dir"
+        --tag "$image" \
+        --cache-to type=local,dest="$cachedir" \
+        --cache-from type=local,src="$cachedir" \
+        "$context" && \
+    # Run a quick test to make sure the image is secure:
+    docker scout quickview "$image"
+
+    echo "✅ $image"
 done
