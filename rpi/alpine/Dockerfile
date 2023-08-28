72e3254a4ebf44b4dc4ff81a43a1457fc93242ec
Joshua Delsman <voxxit@users.noreply.github.com>
2023-08-27 20:01:31 -0400

    Super final

--- rpi/alpine/Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM scratch
-ADD  rootfs.tar.gz /
-CMD  [ "ash" ]
--- rpi/alpine/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -xe
-IMAGE="voxxit/alpine-rpi:latest"
-
-docker build --no-cache --tag $IMAGE `dirname $0`
-docker push $IMAGE
